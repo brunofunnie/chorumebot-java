afc6ef4bcc8b95c1f3230bd4b7e1e2e09ace1068
djonatah <djonatah@gmail.com>
2023-12-31 14:23:55 -0500

    feat - added testing for ComponentScanner.java

---
+++ src/test/tech/chorume/bot/containers/scanner/test/SlashCommandBuilder2NoAnnotation.java
@@ -0,0 +1,18 @@
+package tech.chorume.bot.containers.scanner.test;
+
+import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
+import tech.chorume.bot.core.interfaces.SlashCommandBuilder;
+import tech.chorume.bot.core.interfaces.SlashCommandHandler;
+
+public class SlashCommandBuilder2NoAnnotation implements SlashCommandBuilder {
+
+    @Override
+    public SlashCommandData buildCommand() {
+        return null;
+    }
+
+    @Override
+    public SlashCommandHandler buildHandler() {
+        return null;
+    }
+}
---
+++ src/test/tech/chorume/bot/containers/scanner/test/SlashCommandBuilder3NoInterface.java
@@ -0,0 +1,17 @@
+package tech.chorume.bot.containers.scanner.test;
+
+import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
+import tech.chorume.bot.core.annotations.CommandBuilder;
+import tech.chorume.bot.core.interfaces.SlashCommandHandler;
+
+@CommandBuilder
+public class SlashCommandBuilder3NoInterface {
+
+    public SlashCommandData buildCommand() {
+        return null;
+    }
+
+    public SlashCommandHandler buildHandler() {
+        return null;
+    }
+}
