53340d4c6f0afe5a03deb1fe1fb5a3e2b7577597
djonatah <djonatah@gmail.com>
2024-01-06 04:02:00 -0500

    fix - fixed component scanning when running through docker

--- src/main/java/tech/chorume/bot/App.java
+++ src/main/java/tech/chorume/bot/App.java
@@ -3,7 +3,7 @@ package tech.chorume.bot;
 
 import tech.chorume.bot.core.DiscordBot;
 
-public class App {
+public class  App {
     public static void main(String[] args) {
        var discordBot = new DiscordBot();
        discordBot.start();
--- src/main/java/tech/chorume/bot/core/containers/loader/strategies/CompositeFilter.java
+++ src/main/java/tech/chorume/bot/core/containers/loader/strategies/CompositeFilter.java
@@ -1,8 +1,6 @@
-package tech.chorume.bot.core.containers.scanner.filters;
+package tech.chorume.bot.core.containers.loader.strategies;
 
-import tech.chorume.bot.core.annotations.CommandBuilder;
 import tech.chorume.bot.core.interfaces.ComponentFilter;
-import tech.chorume.bot.core.interfaces.SlashCommandBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,13 +9,14 @@ import java.util.List;
  * Predefined composite filter for Bot commands. Could be turned into a class_and_interface filter
  * if this same logic needs to be applied somewhere else.
  */
-public class BotSlashCommandBuilderFilter implements ComponentFilter {
+public class CompositeFilter implements ComponentFilter {
 
     List<ComponentFilter> filters = new ArrayList<>();
-    public BotSlashCommandBuilderFilter() {
-        filters.add(new AnnotationFilter(CommandBuilder.class));
-        filters.add(new InterfaceImplFilter(SlashCommandBuilder.class));
+
+    public CompositeFilter(List<ComponentFilter> filters) {
+        this.filters.addAll(filters);
     }
+
     @Override
     public boolean match(Class<?> clazz) {
         return filters.stream().allMatch(filter -> filter.match(clazz));
